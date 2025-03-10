f90474c343f449204f6e19c8e3e729468acc273a
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-09 22:31:43 -0600

    Set the http client to use http requests

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/list/country-list.component";
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
   imports: [SearchInputComponent, CountryListComponent],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+  countryService = inject(CountryService);
+}
