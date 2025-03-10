b03c8bf6be88d9d04833e7f559e24e5a84254131
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-09 22:50:10 -0600

    Configure the environment for http request

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,9 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
   private http = inject(HttpClient);
+
+  public searchByCapital(query: string) {
+    query = query.toLocaleLowerCase();
+
+    return this.http.get(`${environment.API_URL}/capital/${query}`);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  companyName: 'Country',
+  companyLastName: 'App',
+
+  // ApiKeys
+  API_URL: 'https://restcountries.com/v3.1',
+};
