3690eed4816e7313ae3ac0b28ebadb35ea17d233
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-12 03:07:16 -0600

    Implement async reactivity with resources

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -11,28 +12,16 @@ import { Country } from '../../interfaces/country.interface';
 })
 export class ByCapitalPageComponent {
   public countryService = inject(CountryService);
+  public query = signal('');
 
-  public isLoading = signal(false);
-  public isError = signal<string | null>(null);
-  public countries = signal<Country[]>([]);
+  public countryResource = resource({
+    request: () => ({query: this.query()}),
+    loader: async({request}) => {
+      if(!request.query) return [];
 
-  public onSearch(query: string) {
-    if(this.isLoading()) return;
-
-    this.isLoading.set(true);
-    this.isError.set(null);
-
-    this.countryService.searchByCapital(query)
-      .subscribe({
-        next: (countries) => {
-          this.isLoading.set(false);
-          this.countries.set(countries);
-        },
-        error: (err) => {
-          this.isLoading.set(false);
-          this.countries.set([]);
-          this.isError.set(err);
-        }
-      });
-  }
+      return await firstValueFrom(
+        this.countryService.searchByCapital(request.query)
+      );
+    },
+  })
 }
