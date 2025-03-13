858a15bbc95915f163a2eb8696dc007790b0562b
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-12 23:28:53 -0600

    Change to rxResource to return an Observable instead Promise

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { of } from 'rxjs';
+
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { CountryService } from '../../services/country.service';
-import { Country } from '../../interfaces/country.interface';
-import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -14,14 +15,11 @@ export class ByCapitalPageComponent {
   public countryService = inject(CountryService);
   public query = signal('');
 
-  public countryResource = resource({
+  public countryResource = rxResource({
     request: () => ({query: this.query()}),
-    loader: async({request}) => {
-      if(!request.query) return [];
-
-      return await firstValueFrom(
-        this.countryService.searchByCapital(request.query)
-      );
+    loader: ({request}) => {
+      if(!request.query) return of([]);
+      return this.countryService.searchByCapital(request.query);
     },
-  })
+  });
 }
