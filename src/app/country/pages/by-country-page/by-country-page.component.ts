08a42f34c356431fdf8f226dc1e37ed9bdb4ce53
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-16 15:20:57 -0600

    Implement queryParams on by-country page section

--- src/app/country/pages/by-country-page/by-country-page.component.ts
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { of } from 'rxjs';
 
 import { CountryListComponent } from "../../components/country-list/country-list.component";
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { CountryService } from '../../services/country.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-by-country-page',
@@ -13,12 +14,22 @@ import { CountryService } from '../../services/country.service';
 })
 export class ByCountryPageComponent {
   public countryService = inject(CountryService);
-  public query = signal('');
+  public activatedRoute = inject(ActivatedRoute);
+  public router = inject(Router);
+  public queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+  public query = linkedSignal(() => this.queryParam);
 
   public countryResource = rxResource({
-    request: () => ({query: this.query()}),
-    loader: ({request}) => {
+    request: () => ({ query: this.query() }),
+    loader: ({ request }) => {
       if(!request.query) return of([]);
+
+      this.router.navigate(['/country/by-country'], {
+        queryParams: {
+          query: request.query,
+        },
+      });
+
       return this.countryService.searchByCountry(request.query);
     },
   });
