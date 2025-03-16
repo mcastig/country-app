133ba6d87146df1e3bfbc7cf2641287af98b4006
Maximino Castillo <maxcastgarcia@gmail.com>
2025-03-15 22:56:59 -0600

    Add debounce for automatic search

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -8,4 +8,18 @@ import { Component, input, output } from '@angular/core';
 export class SearchInputComponent {
   public placeholder = input<string>('Search...');
   public searchValue = output<string>();
+  public inputValue = signal<string>('');
+  public debounceTime = input<number>(300);
+
+  public debounceEffect = effect((onCleanup) => {
+    const value = this.inputValue();
+
+    const timeout = setTimeout(() => {
+      this.searchValue.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(() => {
+      clearTimeout(timeout);
+    });
+  });
 }
